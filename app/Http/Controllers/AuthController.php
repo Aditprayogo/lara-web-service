aff5934552ad3f3bd3509983a1bb9d9e978efeae
Aditprayogo <adit.ihzar@gmail.com>
2019-10-18 14:26:25 +0700

    adding register API logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Hash;
 use App\User;
+use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
@@ -12,6 +13,12 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         # code...
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+
         $user = User::where('email', '=', $request->email)->firstOrFail();
         $status = "error";
         $message = "";
@@ -49,6 +56,52 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         # code...
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255', 
+            'email' => 'required|string|email|max:255|unique:users', 
+            'password' => 'required|string|min:6',
+        ]);
+
+        $status = "error";
+        $message = "";
+        $data = null;
+        $code = 400;
+
+
+        if ($validator->fails()) { 
+            // validasi gagal
+            $errors = $validator->errors();
+            $message = $errors;
+
+        }
+        else{
+            // validasi sukses
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'roles' => json_encode(['CUSTOMER']),
+            ]);
+
+            if ($user) {
+                # code...
+                $user->generateToken();
+                $status = "success";
+                $message = "register successfully";
+                $data = $user->toArray();
+                $code = 200;
+
+            } else {
+
+                $message = 'register failed';
+            }
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
     }
 
     public function logout(Request $request)
